61c4147be0f1676a4af7b2d8e7c216c7150e17ce
Sittipong Hemloun <77152671+ong22280@users.noreply.github.com>
2023-02-16 14:51:57 +0700

    add: readme

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # JWT-react-node-mySQL
+Example using react, nodeJS, mySQL with JWT(Json web token) to create login and register system
+
+## What is a JWT?
+JSON Web Token is an open industry standard used to share information between two entities, usually a client (like your app’s frontend) and a server (your app’s backend).
+
+They contain JSON objects which have the information that needs to be shared. Each JWT is also signed using cryptography (hashing) to ensure that the JSON contents (also known as JWT claims) cannot be altered by the client or a malicious party.
+
+For example, when you sign in with Google, Google issues a JWT which contains the following claims / JSON payload:
+
+[JWT.IO](https://jwt.io/) allows you to decode, verify and generate JWT.
+
+<img src="./screenshots.png" style="border-radius: 20px;"/>
+
+## Prerequisites
+1. Install XAMPP web server
+2. Any Editor (Preferably VS Code or Sublime Text)
+3. Any web browser with latest version
+
+## Languages and Technologies used
+1. HTML5/CSS3
+2. ReactJS
+3. Material-UI
+4. XAMPP
+5. Nodejs express nodemon
+6. MySQL
+
+## Steps to run the project in your machine
+1. import sql script into mysql (mydb.sql)
+2. ### Server Installation
+    ``````````````````````````
+    cd server 
+    npm install
+    npm run dev
+    ``````````````````````````
+    Open [http://localhost:5173/](http://localhost:5173/) to view it in the browser.
+3. ### Server Installation
+    ``````````````````````````
+    cd server 
+    npm install
+    nodemon app.js
+    ``````````````````````````
+    Open [http://localhost:3333](http://localhost:3333) to view it in the browser.
---
+++ mydb.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 16, 2023 at 08:22 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mydb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` text NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `lname` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `fname`, `lname`) VALUES
+(4, 'ong22280@gmail.com', '$2b$10$jF3kAy/hbyhOo.uRWYf2ge6Ml4a6SEy49zLSfLEZZXVZ7i7LDJW2.', 'Sittipong', 'Hemloun');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
